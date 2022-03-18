552c4e9df6b1080377a5429ced1c45a84786aca7
maiconhenriquefa <maiconhenriquefa@gmail.com>
2022-03-18 11:25:06 -0300

    animation login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -6,6 +6,7 @@ import {
   Keyboard,
   Image,
   KeyboardAvoidingView,
+  Animated,
 } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import React, {useState, useEffect} from 'react';
@@ -37,6 +38,7 @@ export default function Login({navigation}) {
         setToken(requestTolken);
       } catch (error) {
         console.log(error);
+        throw error;
       }
     }
     awaitGetToken();
@@ -60,9 +62,14 @@ export default function Login({navigation}) {
       }
     } catch (error) {
       console.log(error);
+      throw error;
     }
   }
-
+  const [bottom, setBottom] = useState(new Animated.Value(800));
+  Animated.timing(bottom, {
+    toValue: 0,
+    duration: 1000,
+  }).start();
   return (
     <KeyboardAvoidingView behavior="position" style={styles.container}>
       <View>
@@ -70,49 +77,71 @@ export default function Login({navigation}) {
           style={styles.banner}
           source={require('../../assets/bannerLogin.png')}
         />
-        <View style={styles.logo}>
-          <Image source={require('../../assets/logo.png')} />
+        <Animated.Image
+          style={{
+            bottom: bottom,
+            height: 130,
+            resizeMode: 'cover',
+            alignSelf: 'center',
+            position: 'absolute',
+          }}
+          source={require('../../assets/logo.png')}
+        />
+      </View>
+      <Animated.View
+        style={{
+          top: bottom,
+        }}>
+        <View style={styles.textEntry}>
+          <Text style={styles.textEntry.login}>Login</Text>
+          <Text style={styles.textEntry.description}>
+            Entre na sua conta para continuar.
+          </Text>
         </View>
-      </View>
-      <View style={styles.textEntry}>
-        <Text style={styles.textEntry.login}>Login</Text>
-        <Text style={styles.textEntry.description}>
-          Entre na sua conta para continuar.
-        </Text>
-      </View>
-      <View style={styles.inputArea}>
-        <EvilIcons style={styles.userIcon} name="user" color="#fff" size={20} />
-        <TextInput
-          keyboardType={'email-address'}
-          autoComplete={'email'}
-          style={styles.input}
-          placeholder="e-mail"
-          onChangeText={usernameInput => {
-            setUsername(usernameInput);
-          }}
-          placeholderTextColor="rgba(255, 255, 255, 0.5)"
-        />
-        <AntDesign style={styles.lockIcon} name="lock" color="#fff" size={18} />
-        <TextInput
-          onChangeText={passwordInput => {
-            setPassword(passwordInput);
-          }}
-          autoComplete={'password'}
-          style={styles.input}
-          placeholder="senha"
-          placeholderTextColor="rgba(255, 255, 255, 0.5)"
-          secureTextEntry={true}
-        />
-      </View>
-      <View style={styles.btnSubmit}>
-        <TouchableOpacity
-          onPress={() => {
-            Keyboard.dismiss();
-            isSucess(user);
-          }}>
-          <Text style={styles.btnSubmit.text}>Entrar</Text>
-        </TouchableOpacity>
-      </View>
+        <View style={styles.inputArea}>
+          <EvilIcons
+            style={styles.userIcon}
+            name="user"
+            color="#fff"
+            size={20}
+          />
+          <TextInput
+            keyboardType={'email-address'}
+            autoComplete={'email'}
+            style={styles.input}
+            placeholder="e-mail"
+            onChangeText={usernameInput => {
+              setUsername(usernameInput);
+            }}
+            placeholderTextColor="rgba(255, 255, 255, 0.5)"
+          />
+          <AntDesign
+            style={styles.lockIcon}
+            name="lock"
+            color="#fff"
+            size={18}
+          />
+          <TextInput
+            onChangeText={passwordInput => {
+              setPassword(passwordInput);
+            }}
+            autoComplete={'password'}
+            style={styles.input}
+            placeholder="senha"
+            placeholderTextColor="rgba(255, 255, 255, 0.5)"
+            secureTextEntry={true}
+          />
+        </View>
+        <View style={styles.btnSubmit}>
+          <TouchableOpacity
+            onPress={() => {
+              Keyboard.dismiss();
+              isSucess(user);
+            }}>
+            <Text style={styles.btnSubmit.text}>Entrar</Text>
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
     </KeyboardAvoidingView>
   );
 }
