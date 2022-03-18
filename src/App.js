95016396bb503a09ca5295099b163262c7f5c06b
maiconhenriquefa <maiconhenriquefa@gmail.com>
2022-03-18 11:24:46 -0300

    warn disabled for apresentation

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import Movies from './pages/Movies';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {LogBox} from 'react-native';
-LogBox.ignoreLogs([
-  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
-]);
+// LogBox.ignoreLogs([
+//   "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+// ]);
+LogBox.ignoreAllLogs();
 
 const Stack = createStackNavigator();
 
