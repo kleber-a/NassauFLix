54e587ab959ea81715470ce8a5d71c124cfdf637
Feliciano Brancarto <feliciano.brancarto@gmail.com>
2022-03-23 14:50:20 -0300

    styling Favorites TvShows

--- src/pages/TvShow/index.js
+++ src/pages/TvShow/index.js
@@ -2,35 +2,45 @@ import React from 'react';
 import { View, Text, TouchableOpacity, Image, FlatList } from 'react-native';
 import styles from './styles';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import style from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 export default function TvShow({navigation}) {
-    const lista = [1,2,3,4,5,6,7,8,9,10]
+    const lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
  return (
      <>
-    <View>
+    <View style={style.container}>
         <TouchableOpacity
-          style={{color:"red"}}
+          style={style.return}
           onPress={() => navigation.goBack()}>
-          <AntDesign style={{color:"red"}} name="arrowleft" size={25} />
+          <AntDesign style={style.button} name="arrowleft" size={25} />
         </TouchableOpacity>
-    <Text>Filmes favoritos do <Text style={{color:"pink"}}>John!</Text></Text>
+        <View style={style.boxHeader}>
+        <Text style={style.textFavoritesTvShow}>Filmes favoritos do <Text style={{color:"pink"}}>John!</Text></Text>
+        </View>
     
+    <View style={style.boxFlatList}>
     <FlatList
-    
+        
         data={lista}
         numColumns={4}
         keyExtractor={(item, index) => item}
 
         renderItem={item => 
              <View>
-                 <Image source={require("./aranha.png")}/>
-                <Text style={{color:"black"}}>Nota</Text>
+                <Image style={style.Image} source={require("./aranha.png")}/>
+                <View style={style.containerAvaluation}>
+                <Icon style={style.icon} name="star" />
+                <Text style={style.avaluationstyle}>8/10</Text>
+          </View>
              </View>
 
         }
    
     />
+        </View>
+
 
   </View>
   </>
--- src/pages/TvShow/styles.js
+++ src/pages/TvShow/styles.js
@@ -1,9 +1,71 @@
 import {StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: '#000000',
+    },
+    textFavoritesTvShow:{
+        color:"#ffffff",
+        fontFamily:"open sans",
+        fontSize:20,
+    },
+    boxHeader:{
+        
+        height:"10%",
+        width:"100%",
+        marginVertical:"5%",
+        alignItems:"center",
+        justifyContent:"center",
+        
+        
+    },
+    return:{
+        backgroundColor:"#ffffff",
+        borderRadius:15,
+        width:30,
+        height:30,
+        alignItems:"center",
+        justifyContent:"center",
+        marginTop:25,
+        left:15
 
+    },
+    button:{
+        color:"#000000",
+    },
 
-    
+    boxFlatList:{
+        marginLeft:17,
+        height:"100%", 
+        justifyContent:"center",
+       
+    },
+    containerAvaluation: {
+        alignSelf: 'flex-start',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        marginTop: 4,
+      },
+      avaluationstyle: {
+        flexDirection: 'row',
+        fontSize: 12,
+        fontWeight: '600',
+        color: '#FFFFFF',
+        marginLeft: 8,
+        fontFamily: 'open sans',
+      },
+      icon: {
+        color: '#EC2626',
+        fontSize: 12,
+      },
+      Image:{
+         height:95,
+         width:75,
+         justifyContent: "center",
+         margin:"3%"
+      },
+
 });
 
-export default styles;
+export default style;
