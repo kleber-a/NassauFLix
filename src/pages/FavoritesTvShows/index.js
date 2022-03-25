9da2fba4ce54001692baa300b40896f973228ae0
Feliciano Brancarto <feliciano.brancarto@gmail.com>
2022-03-25 14:52:53 -0300

    factoring username

--- src/pages/FavoritesTvShows/index.js
+++ src/pages/FavoritesTvShows/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import {View, Text, TouchableOpacity, Image, FlatList} from 'react-native';
 import styles from './styles';
 import AntDesign from 'react-native-vector-icons/AntDesign';
@@ -6,21 +6,33 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import {getAccountDetails, getFavoritesTvShows} from '../../service/api';
 import AsyncStorage from '@react-native-community/async-storage';
 import MovieImage from '../../components/Movie/MovieImage';
+import {AuthContext} from '../../context/auth';
 
 export default function FavoritesTvShows({navigation}) {
   const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
   const [favoritesTvShows, setfavoritesTvShows] = useState([]);
+  const {account} = useContext(AuthContext);
+  const[name, setName] = useState();
+  
+
+  
 
   useEffect(() => {
     async function awaitFavoritesTvShows() {
       try {
         const sessionId = await AsyncStorage.getItem('@CodeApi:session');
-        const account = await getAccountDetails(sessionId);
         const favoritesTvShows = await getFavoritesTvShows(
           account.id,
           sessionId,
         );
         setfavoritesTvShows(favoritesTvShows);
+        if(account.name) {
+            setName(account.name)
+    
+        }
+        else{
+            setName(account.username)
+        }
       } catch (error) {
         console.warn(error);
       }
@@ -38,14 +50,14 @@ export default function FavoritesTvShows({navigation}) {
         </TouchableOpacity>
         <View style={styles.boxHeader}>
           <Text style={styles.textFavoritesTvShow}>
-            Séries favoritas do <Text style={{color: 'pink'}}>John!</Text>
+            Séries favoritas do <Text style={{color: 'pink'}}>{name}!</Text>
           </Text>
         </View>
 
           <FlatList
             data={favoritesTvShows.results}
             numColumns={4}
-            keyExtractor={(item, index) => item}
+            keyExtractor={(item, index) => item.id}
             renderItem={item => (
               <View style={styles.boxImage}>
                 <MovieImage
