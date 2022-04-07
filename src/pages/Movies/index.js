234b6f93bf3228555baf732b73521e3c9fccd81f
Soufelipeee <98565012+Soufelipeee@users.noreply.github.com>
2022-04-07 00:08:15 -0300

    Deleted unused lines

--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,89 +1,28 @@
-import { View, Text, TouchableOpacity, Image, FlatList } from 'react-native';
-import React, { useState, useEffect, useContext } from 'react';
+import { View, Text, Image, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import Loading from '../../components/Loading';
 import {
   getCredits,
-  getDetails,
-  getState,
-  postFavorite,
 } from '../../service/api';
 import styles from './styles';
-import { AuthContext } from '../../context/auth';
 import HeaderMoviesOrSeriesDetails from '../../components/HeaderMoviesOrSeriesDetails'
 
-
 export default function Movies({ route, navigation }) {
-  const [id, type] = route.params;
-  const [details, setDetails] = useState([]);
+  const [id] = route.params;
   const [cast, setCast] = useState(null);
-  const [crew, setCrew] = useState(null);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [isRated, setIsRated] = useState(false);
-  const [movieRated, setMovieRated] = useState(null);
 
-  const [isFavorite, setIsFavorite] = useState(null);
-  const [dataFavorite, setDataFavorite] = useState({
-    media_type: 'movie',
-    media_id: id,
-    favorite: false,
-  });
-  const { sessionId, account } = useContext(AuthContext);
-
-  async function awaitFavoriteMovies() {
-    try {
-      await postFavorite(account.id, sessionId, dataFavorite);
-    } catch (error) {
-      console.warn(error);
-    }
-  }
-
-  useEffect(() => {
-    async function awaitIsFavorite(bodyfavorite) {
-      const { favorite } = await getState('movie', id, sessionId);
-      setIsFavorite(favorite);
-      setDataFavorite(prevState => ({ ...prevState, favorite: !favorite }));
-    }
-    awaitIsFavorite();
-  }, [id, sessionId]);
-
-  useEffect(() => {
-    async function awaitGetDetails() {
-      try {
-        const dataDetails = await getDetails(id);
-        setDetails(dataDetails);
-      } catch (error) {
-        console.warn(error);
-      }
-    }
-    awaitGetDetails();
-  }, [id]);
 
   useEffect(() => {
     async function awaitGetCredits() {
       try {
         const dataCredits = await getCredits(id);
         setCast(dataCredits.cast);
-        setCrew(dataCredits.crew);
       } catch (error) {
         console.warn(error);
       }
     }
     awaitGetCredits();
   }, [id]);
-
-  async function awaitAvaluates() {
-    try {
-      const stateMovie = await getState(type, id, sessionId);
-      setMovieRated(stateMovie.rated.value);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    awaitAvaluates();
-  }, [isRated]);
-
 
   const renderItem = ({ item }) => {
     return (
