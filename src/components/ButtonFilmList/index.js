0e8fe5e7bf4e65b2041fa63d409b188624433b9e
Soufelipeee <98565012+Soufelipeee@users.noreply.github.com>
2022-04-08 16:22:17 -0300

    Added ComponentButtonFilmeList to PageProfile

--- src/components/ButtonFilmList/index.js
+++ src/components/ButtonFilmList/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import styles from './styles'
 
-export default function ButtonFilmList({ navigation }) {
+export default function ButtonFilmList({ navigation, navigate }) {
 
     return (
-        <>
-            <TouchableOpacity
-                style={styles.containerButton}
-            // onPress={() => navigation.navigate()}
-            >
-                <Text style={styles.textButton}>Ver Lista de Filmes</Text>
-            </TouchableOpacity>
-        </>
-
+        <TouchableOpacity
+            style={styles.containerButton}
+            onPress={() => {
+                navigation.navigate(navigate)
+            }}
+        >
+            <Text style={styles.textButton}>Ver Lista de Filmes</Text>
+        </TouchableOpacity>
     )
 };
 
